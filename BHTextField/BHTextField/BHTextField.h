86744822ebae052ad5101ea217f5a571d378eb41
To <nattapong.u@23perspective.com>
2018-05-21 18:40:51 +0700

    project in new position

---
+++ BHTextField/BHTextField/BHTextField.h
@@ -0,0 +1,19 @@
+//
+//  BHTextField.h
+//  BHTextField
+//
+//  Created by Nattapong Unaregul on 21/5/18.
+//  Copyright © 2018 Nattapong Unaregul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BHTextField.
+FOUNDATION_EXPORT double BHTextFieldVersionNumber;
+
+//! Project version string for BHTextField.
+FOUNDATION_EXPORT const unsigned char BHTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BHTextField/PublicHeader.h>
+
+
